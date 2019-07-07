e89dbd9ac6e453fad37f6c4f4e7f7624a388ef0a
Ronnie Chang <yuugou727@gmail.com>
2019-07-07 17:37:06 +0800

    update npm package(audit) and firebase package

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
         <Clock/>
         <Weather/>
         <footer>
-          <p>Built & hosted: <a href="https://github.com/yuugou727" target="_blank" rel="noopener noreferrer">Ronnie Chang</a> Weather API: <a href="https://openweathermap.org/" target="_blank" rel="noopener noreferrer">openweathermap.org</a></p>
+          <p>Built & hosted: <a href="https://github.com/yuugou727" target="_blank" rel="noopener noreferrer">Ronnie Chang</a>. Weather API: <a href="https://openweathermap.org/" target="_blank" rel="noopener noreferrer">openweathermap.org</a></p>
+          <span className="version">v20190707</span>
         </footer>
         <ToastContainer position="bottom-center" hideProgressBar newestOnTop={false} closeOnClick rtl={false} pauseOnHover/>
       </div>
